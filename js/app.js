ebe102a554cb953cce087711f568a07533d219c1
Ruslan Posevkin <rus.posevkin@gmail.com>
2017-01-26 01:04:25 +0300

    update locations list (7 items now)

--- js/app.js
+++ js/app.js
@@ -11,21 +11,45 @@ var locationsList = [
     title: 'Bronze Horseman',
     lat: 59.933905,
     long: 30.306485
+  },
+  {
+    title: 'Church of the Savior on Blood',
+    lat: 59.940100,
+    long: 30.3289
+  },
+  {
+    title: 'Hermitage Museum',
+    lat: 59.939832,
+    long: 30.31456
+  },
+  {
+    title: 'Rostral Columns',
+    lat: 59.944682,
+    long: 30.304971
+  },
+  {
+    title: 'Kazan Cathedral',
+    lat: 59.934209,
+    long: 30.316672
+  },
+  {
+    title: 'Peter and Paul Fortress',
+    lat: 59.950002,
+    long: 30.324571
   }
 ];
-var latitude = 59.933905;
-var longitude = 30.306485;
-var query = 'Church of the Savior on Blood';
-var url = 'https://api.foursquare.com/v2/venues/search?v=20161016&ll='
-  + latitude + ',' + longitude + '&intent=global&query=' + query
-  + '&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET;
 
+locationsList.forEach(function(item) {
+  var url = 'https://api.foursquare.com/v2/venues/search?v=20161016&ll='
+    + item.lat + ',' + item.long + '&intent=global&query=' + item.title
+    + '&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET;
 
-$.getJSON(url).done(function(data) {
-  console.log(data.response.venues[0]);
-}).fail(function() {
-  console.error('There was an error occured with the Foursquare API. Please try again later.');
-});
+  $.getJSON(url).done(function(data) {
+    console.log(data.response.venues[0]);
+  }).fail(function() {
+    console.error('There was an error occured with the Foursquare API. Please try again later.');
+  });
+})
 
 var AppViewModel = function() {
     this.data = ko.observableArray(['foo', 'bar']);
